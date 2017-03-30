926442dbea25df1b0373ac9ef9f2edb6f14afd97
王金旺 <jwwangchn@outlook.com>
2017-03-30 20:21:49 +0800

    同步

---
+++ VS/VS/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+
+	return 0;
+}
+
+vector<double> ranking(vector<int> ObjV)
+{
+	vector<double> FitnV;
+	return FitnV;
+}
+
+vector<vector<int>> select(vector<vector<int>> Chrom, vector<double>, double GGAP)
+{
+	vector<vector<int>> SelCh;
+	return SelCh;
+}
+
+vector<vector<int>> recombin(vector<vector<int>> SelCh, double px)
+{
+	return SelCh;
+}
+
+vector<vector<int>> mut(vector<vector<int>> SelCh, double pm)
+{
+	return SelCh;
+}
+
+pair<vector<vector<int>>,vector<int>> reins(vector<vector<int>> SelCh, double pm)
+{
+	pair<vector<vector<int>>, vector<int>> result;
+	return result;
+}
